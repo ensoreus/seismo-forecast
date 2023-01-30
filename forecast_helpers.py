974f5f84928669728112ac5d77b1763b68e8fb7b
Pylyp Maliuta <pylyp.maliuta@stuzo.com>
2023-01-30 16:40:23 +0200

    gather data; making datasets; training a baseline LSTM model

---
+++ forecast_helpers.py
@@ -0,0 +1,107 @@
+import obspy.clients.fdsn
+from obspy.core import read
+from obspy.clients import seedlink
+from obspy import UTCDateTime
+
+import pandas as pd
+import numpy as np
+
+sec_in_hour = 60 * 60
+sec_in_year = 2592000 * 12
+year_to_start =  2008
+
+HORIZON = 1
+WINDOW_SIZE = 7 * 24 * 2
+
+def pick_origin_for_hour(timestamp, catalog):
+  time=UTCDateTime(timestamp)
+  time.minute = 0
+  time.second = 0
+  start_time_filter = UTCDateTime(timestamp)
+  if (start_time_filter.hour - 1) > 0 :
+    start_time_filter.hour = start_time_filter.hour - 1
+  start_time_filter_str = f"{start_time_filter.year}-{start_time_filter.month}-{start_time_filter.day}T{start_time_filter.hour}:59:59.59"
+
+  end_time_filter = UTCDateTime(timestamp)
+  end_time_filter_str = f"{end_time_filter.year}-{end_time_filter.month}-{end_time_filter.day}T{end_time_filter.hour}:59:59.59"
+  filtered = catalog.filter(f"time >= {start_time_filter_str}", f"time <= {end_time_filter_str}")
+  return filtered
+
+def pack_timeline(catalog, timeline):
+      
+  if timeline==None:
+    timeline = {}
+
+  first_event_time = UTCDateTime.now().timestamp - 7 * 24 * 60 * 60  #  catalog.events[-1].origins[-1].time
+  print(f"timestamp now:{UTCDateTime.now().timestamp} firsteventtime:{first_event_time}")
+  last_event_time = catalog.events[0].origins[0].time
+  print(first_event_time, last_event_time)
+  for hour in range(int(first_event_time), int(UTCDateTime.now().timestamp), int(sec_in_hour / 2)):
+    events = pick_origin_for_hour(hour, catalog)
+    if events.count() > 0 and len(events[0].magnitudes) > 0:
+      print(f"hour: {hour} event: {events[0]} mags: {events[0].magnitudes}")
+      timeline[hour] = events[0].magnitudes[0].mag
+    else:
+      timeline[hour] = 0
+  return timeline
+
+def get_labelled_windows(x, horizon=HORIZON):
+  """
+  Creates labels for windowed dataset.
+
+  E.g. if horizon=1
+  Input: [0, 1, 2, 3, 4, 5, 6, 7] -> Output: ([0, 1, 2, 3, 4, 5, 6], [7])
+  """
+
+  return x[:, :-horizon], x[:, -horizon:]
+
+# Create function to view numpy arrays as windows
+def make_windows(x, window_size=WINDOW_SIZE, horizon=HORIZON):
+  """
+  Turns a 1D array into a 2D array of sequential labelled windows of window_size with horizon size labels.
+  """
+  # 1. Create a window of a specific window_size (add teh horizon on the end for labelling later)
+  window_step = np.expand_dims(np.arange(window_size + horizon), axis=0)
+  # 2. Create a 2D array of multiple window steps (minus 1 to account for 0 indexing)
+  window_indexes = window_step + np.expand_dims(np.arange(len(x) - (window_size + horizon - 1)), axis=0).T
+  # 3. INdex on the target array (a time series) with 2D arrat nultiple window steps
+  windowed_array = x[window_indexes]
+  # 4. Get the labelled windows
+  windows, labels = get_labelled_windows(windowed_array, horizon=horizon)
+
+  return windows, labels
+
+# Make the train/test splits
+def make_train_test_splits(windows, labels, test_split=0.2):
+  """
+  Splits matching pairs of windows and labels into train and test splits
+  """
+  split_size = int(len(windows) * (1 - test_split)) # this will default to 80% train/20% test data
+  train_windows = windows[:split_size]
+  train_labels = labels[:split_size]
+  test_windows = windows[split_size:]
+  test_labels = labels[split_size:]
+  return train_windows, test_windows, train_labels, test_labels
+
+def pack_timeline(catalog, timeline):
+  
+  if timeline==None:
+    timeline = {}
+
+  first_event_time = UTCDateTime.now().timestamp - 7 * 24 * 60 * 60 # a week ago in sec
+  last_event_time = catalog.events[0].origins[0].time
+  print(first_event_time, last_event_time)
+  for hour in range(int(first_event_time), int(UTCDateTime.now().timestamp), int(sec_in_hour / 2)):
+    events = pick_origin_for_hour(hour, catalog)
+    if events.count() > 0 and len(events[0].magnitudes) > 0:
+      print(f"hour: {hour} event: {events[0]} mags: {events[0].magnitudes}")
+      timeline[hour] = events[0].magnitudes[0].mag
+    else:
+      timeline[hour] = 0
+  return timeline 
+
+def prepare_window(timeline):
+  mags = np.fromiter(timeline.values(), dtype=np.float64)
+  times = np.fromiter(timeline.keys(), dtype=np.float64)
+  return times, mags
+
---
+++ train.py
@@ -0,0 +1,109 @@
+import obspy.clients.fdsn
+from obspy.core import read
+from obspy.clients import seedlink
+from obspy import UTCDateTime
+
+import tensorflow as tf
+import forecast_helpers as fh
+import pandas as pd
+import numpy as np
+
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+sec_in_hour = 60 * 60
+sec_in_year = 2592000 * 12
+year_to_start =  2008
+WINDOW_SIZE=7*24*2 # half-hours in a week
+HORIZON=1 # next 30 minutes
+
+def prepare_datasets():
+
+    loaded_timeline = load_datasets("mags-1998-2022.csv")
+    if loaded_timeline.empty:
+        client=obspy.clients.fdsn.Client("EMSC")
+
+        final_time = int(UTCDateTime.now().timestamp)
+        initial_time = int(UTCDateTime(year_to_start, 1, 1, 0, 0).timestamp)
+        mags_timeline = None
+        step = int(sec_in_year / 6)
+
+        for leap in range(initial_time, final_time, step):
+          starttime = leap
+          endtime = leap + step
+          catalog = client.get_events(starttime=starttime,
+                            endtime=endtime,
+                            latitude=45.87410,
+                            longitude=26.12301,
+                            maxradius=5)
+
+          mags_timeline = fh.pack_timeline(catalog, mags_timeline)
+          print(f"A {catalog.count()} events happened in Romania region ever since {UTCDateTime(initial_time).date} till {UTCDateTime(leap).date}")
+          timeline = pd.DataFrame(mags_timeline.items())
+    else:
+        timeline = loaded_timeline.drop(labels='Unnamed: 0', axis=1)
+        
+    timeline.columns= ['time', 'mag']
+
+    timeline['time'] = timeline['time'].apply(lambda event: datetime.fromtimestamp(event))
+
+    timeline_mag = timeline["mag"].to_numpy()
+    timeline_time = timeline["time"].to_numpy()
+
+    full_windows, full_labels = fh.make_windows(timeline_mag, window_size=WINDOW_SIZE, horizon=HORIZON)
+
+    # Create train / test windows
+    train_windows, test_windows, train_labels, test_labels = fh.make_train_test_splits(full_windows, full_labels)
+    return train_windows, test_windows, train_labels, test_labels
+
+def load_datasets(filename):
+    timeline =pd.read_csv(filename)
+    return timeline
+
+def make_model(name):
+    expand_dims_layer = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=1))
+
+    input = tf.keras.layers.Input(shape=(WINDOW_SIZE,), dtype=tf.float64)
+    x = expand_dims_layer(input)
+    x = tf.keras.layers.LSTM(128, activation=tf.keras.activations.relu)(x)
+    #x = tf.keras.layers.LSTM(32, activation=tf.keras.activations.relu)(x)
+    output = tf.keras.layers.Dense(HORIZON, activation=tf.keras.activations.relu)(x)
+    model = tf.keras.models.Model(input, output, name=name)
+
+    model.compile(loss=tf.keras.losses.mae, 
+                    optimizer=tf.keras.optimizers.Adam(),
+                    metrics=["mae", "mse"])
+    
+    return model
+
+def fit_model(model, train_windows, test_windows, train_labels, test_labels, do_plot=False):
+    checkpoints_path = f"{model.name}.ckpt"
+    checkout_callback = tf.keras.callbacks.ModelCheckpoint(checkpoints_path,
+        save_weights_only=True,
+        save_best_only=True,
+        verbose=1)
+    tensorbaord_callback = tf.keras.callbacks.TensorBoard("logs")
+    history = model.fit(train_windows,
+                    train_labels,
+                    epochs=5, validation_data=(test_windows, test_labels),
+                    batch_size=WINDOW_SIZE,
+                    callbacks=[checkout_callback, tensorbaord_callback]
+                    )
+    model.load_weights(checkpoints_path)
+    if do_plot:
+        plt.figure(figsize=(10, 7))
+        plt.plot(history.history["mae"])
+        plt.show()
+    return model
+
+def evaluate(model):
+    model.evaluate(test_windows,  test_labels)
+    
+if __name__ == "__main__":
+    print("Preparing datasets...")
+    train_windows, test_windows, train_labels, test_labels = prepare_datasets()
+    print("Building a model")
+    model = make_model("EtQForecast-LSTM-week-horizon-30min")
+    print("Fitting the model")
+    fit_model(model, train_windows, test_windows, train_labels, test_labels,  do_plot=True)
+    model.save(model.name)
